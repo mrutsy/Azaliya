19c1fe881a8b931d3f8ba842b07342118192b674
mrutsy <roman@zhilk.in>
2023-02-23 22:53:28 +0700

    Play music on Python.

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import os
+from pydub import AudioSegment
+from pydub.playback import play
+
 
 if __name__ == '__main__':
-    print("Start AzaliyaApp")
-    os.system("ls")
+    song = AudioSegment.from_mp3("data/start.mp3")
+    # song = AudioSegment.from_wav("audio_file.wav")
+    # you can also read from other formats such as MP4
+    # song = AudioSegment.from_file("audio_file.mp4", "mp4")
+    play(song)
 
