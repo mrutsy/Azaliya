b92428aecee5af7b4718ff2302fdfe9176f92669
mrutsy <roman@zhilk.in>
2023-03-01 09:31:00 +0700

    First release.

--- main.py
+++ main.py
@@ -4,9 +4,28 @@ from pydub.playback import play
 
 
 if __name__ == '__main__':
-    song = AudioSegment.from_mp3("data/start.mp3")
-    # song = AudioSegment.from_wav("audio_file.wav")
-    # you can also read from other formats such as MP4
-    # song = AudioSegment.from_file("audio_file.mp4", "mp4")
+    # song = AudioSegment.from_mp3("data/start.mp3")
+    # # song = AudioSegment.from_wav("audio_file.wav")
+    # # you can also read from other formats such as MP4
+    # # song = AudioSegment.from_file("audio_file.mp4", "mp4")
+    # play(song)
+    song = AudioSegment.from_ogg("data/start.ogg")
+    play(song)
+    song = AudioSegment.from_ogg("data/about_me.ogg")
     play(song)
 
+    def play_azaliya(url):
+        music = AudioSegment.from_mp3(url)
+        play(music)
+
+    dir_music = os.scandir("data/music")
+    while True:
+        for _ in dir_music:
+            play_azaliya(os.path.join(_))
+        # print(os.path.join("data", "music", _))
+        # print(_)
+    # print(dir_music)
+
+    # while True:
+    #     pass
+    #
