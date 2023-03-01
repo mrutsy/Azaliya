81277bf2ac35eb2e6d23e4ab9594fec32ac8be60
mrutsy <roman@zhilk.in>
2023-03-01 12:56:48 +0700

    test fix

---
+++ main2.py
@@ -0,0 +1,13 @@
+import os
+from pydub import AudioSegment
+from pydub.playback import play
+
+
+if __name__ == '__main__':
+    # song = AudioSegment.from_mp3("data/start.mp3")
+    # # song = AudioSegment.from_wav("audio_file.wav")
+    # # you can also read from other formats such as MP4
+    # # song = AudioSegment.from_file("audio_file.mp4", "mp4")
+    # play(song)
+
+    song = AudioSegment.from_ogg(os.path.join(os.getcwd(), "data/start.ogg"))
---
+++ run2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+: '
+Test
+'
+
+
+check_venv(){
+  echo "Checking VENV..."
+  if [ -d venv ];
+    then
+      echo "VENV - OK!"
+      source venv/bin/activate
+      python -m ensurepip --upgrade
+      pip --version
+      pip install -r /opt/Azaliya/modules.txt
+      python /opt/Azaliya/main2.py
+    else
+      echo "Don't found VENV. Create VENV."
+      python -m venv venv
+      check_venv
+  fi
+}
+
+
+check_venv
+
+#exec python -m pip install --upgrade pip
+#
+#source venv/bin/activate
+#python main.py
